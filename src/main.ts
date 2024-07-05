e2c202d833a0d228d651d49a799aefa174a64a5e
amsraimar <amsraimar@gmail.com>
2024-07-05 08:46:56 -0300

    Elementplus

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,24 @@ import router from './router'
 import { UsuarioServiceImpl, EmpresaServiceImpl } from './service/impl/'
 import { UsuarioServiceKey, EmpresaServiceKey } from './service/'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import ptBr from 'element-plus/es/locale/lang/pt-br'
+import 'dayjs/locale/pt-br'
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
 
 app.use(router)
+
+app.use(ElementPlus).use(ElementPlus, {
+  locale: ptBr
+})
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.provide(UsuarioServiceKey, new UsuarioServiceImpl())
 app.provide(EmpresaServiceKey, new EmpresaServiceImpl())
--- src/views/EmpresaDetalheView.vue
+++ src/views/EmpresaDetalheView.vue
@@ -57,7 +57,7 @@ onMounted(() => {
   <main>
     <Form
       :model="formState"
-      layout="horizontal"
+      layout="vertical"
       @finish="onFinish"
       @finishFailed="onFinishFailed"
       :validate-messages="validateMessages"
